48000fac89963778ba0adfda176bc08e8f892722
loncarevicpedja <loncarevicpedja2000@gmail.com>
2023-09-02 19:34:12 +0200

    finish

--- api/index.js
+++ api/index.js
@@ -19,7 +19,12 @@ dotenv.config();
 const salt = bcrypt.genSaltSync(10);
 const secret = "dsad32dq";
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(
+  cors({
+    credentials: true,
+    origin: "https://mern-app-frontend-nine.vercel.app",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
