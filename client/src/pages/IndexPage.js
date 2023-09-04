86f9e5769227f4250ae5fd869621b09f7c2b3da7
loncarevicpedja <92603394+loncarevicpedja@users.noreply.github.com>
2023-09-04 09:15:37 +0200

    Update IndexPage.js

--- client/src/pages/IndexPage.js
+++ client/src/pages/IndexPage.js
@@ -13,7 +13,7 @@ export default function IndexPage() {
     setShowAnimation(true);
   }, []);
 
-  const url = "http://mern-app-api.vercel.app";
+  const url = "http://localhost:4000";
   useEffect(() => {
     fetch(`${url}/exhibition`)
       .then((response) => response.json())
